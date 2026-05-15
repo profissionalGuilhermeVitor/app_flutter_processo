98e4f053a2001a4776df56bcc0e64d82408615b0
profissionalGuilhermeVitor <vitorb444@gmail.com>
2026-05-15 14:50:45 -0300

    Resolução de erros

---
+++ app/lib/screens/first_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class PrimeiraPagina extends StatelessWidget{
+  const PrimeiraPagina({super.key});
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      
+    );
+  }
+}
